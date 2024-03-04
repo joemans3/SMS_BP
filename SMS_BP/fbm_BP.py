8551b231e80ba60162999f1d0100b07223a5d320
Baljyot Parmar <joemans3@users.noreply.github.com>
2024-03-04 06:01:23 -0500

    FIX: multiple dimensions of fbm now have the same diffusion/hurst statistic

--- SMS_BP/fbm_BP.py
+++ SMS_BP/fbm_BP.py
@@ -5,10 +5,12 @@ BOUNDARY_CONDITIONS = {
     'reflecting': _refecting_boundary,
     'absorbing': _absorbing_boundary
 }
-def MCMC_state_selection(initial_state_index:int, 
-                         transition_matrix:np.ndarray,
-                         possible_states:np.ndarray,
-                         n:int):
+
+
+def MCMC_state_selection(initial_state_index: int,
+                         transition_matrix: np.ndarray,
+                         possible_states: np.ndarray,
+                         n: int):
     '''Markov Chain Monte Carlo state selection
 
     Parameters:
@@ -21,64 +23,74 @@ def MCMC_state_selection(initial_state_index:int,
         possible states
     n : int
         Number of iterations
-    
+
     Returns:
     --------
     np.ndarray
         State selection at each iteration
     '''
-    #initialize the state selection
+    # initialize the state selection
     state_selection = np.zeros(n)
-    #initialize the current state
+    # initialize the current state
     current_state = possible_states[initial_state_index]
     current_state_index = initial_state_index
-    #find the total rate constant for each state (cache the total rate constant given a state so it does not have to be recalculated)
+    # find the total rate constant for each state (cache the total rate constant given a state so it does not have to be recalculated)
     total_rate_constant = np.zeros(len(possible_states))
     for i in range(len(possible_states)):
         total_rate_constant[i] = np.sum(transition_matrix[i])
     t = 0
     while t < n:
-        #find the rate constant for transitioning from the current state
+        # find the rate constant for transitioning from the current state
         tot_rate_transition = total_rate_constant[current_state_index]
-        #if tot_rate_transition is 0 then the current state is the only state viable based on the transition matrix
+        # if tot_rate_transition is 0 then the current state is the only state viable based on the transition matrix
         if tot_rate_transition == 0:
             state_selection[int(t):n] = current_state
             break
-        #find the time to transition
+        # find the time to transition
         tau = -np.log(np.random.rand())/tot_rate_transition
-        #find the next state
-        next_state_index = np.random.choice(len(possible_states), p=transition_matrix[current_state_index]/tot_rate_transition)
+        # find the next state
+        next_state_index = np.random.choice(len(
+            possible_states), p=transition_matrix[current_state_index]/tot_rate_transition)
         next_state = possible_states[next_state_index]
-        #for the duration of the transition, the state is the previous state
+        # for the duration of the transition, the state is the previous state
         state_selection[int(np.floor(t)):int(np.ceil(t+tau))] = current_state
-        #update the current state
+        # update the current state
         current_state = next_state
         current_state_index = next_state_index
-        #update the time
+        # update the time
         t += int(np.ceil(tau))
     return state_selection
 
+
 class FBM_BP:
     def __init__(self,
-                 n:int,
-                 dt:float,
-                 diffusion_parameters:np.ndarray,
-                 hurst_parameters:np.ndarray,
-                 diffusion_parameter_transition_matrix:np.ndarray,
-                 hurst_parameter_transition_matrix:np.ndarray,
-                 state_probability_diffusion:np.ndarray,
-                 state_probability_hurst:np.ndarray,
-                 space_lim:np.ndarray):
+                 n: int,
+                 dt: float,
+                 diffusion_parameters: np.ndarray,
+                 hurst_parameters: np.ndarray,
+                 diffusion_parameter_transition_matrix: np.ndarray,
+                 hurst_parameter_transition_matrix: np.ndarray,
+                 state_probability_diffusion: np.ndarray,
+                 state_probability_hurst: np.ndarray,
+                 space_lim: np.ndarray):
         self.n = int(n)
-        self.dt = dt#ms
+        self.dt = dt  # ms
         self.diffusion_parameter = diffusion_parameters
         self.hurst_parameter = hurst_parameters
-        self.diffusion_parameter_transition_matrix = diffusion_parameter_transition_matrix #stocastic rate constants with units 1/dt
-        self.hurst_parameter_transition_matrix = hurst_parameter_transition_matrix #stocastic rate constants with units 1/dt
-        self.state_probability_diffusion = state_probability_diffusion #probability of the initial state, this approximates the population distribution
-        self.state_probability_hurst = state_probability_hurst #probability of the initial state, this approximates the population distribution
-        self.space_lim = np.array(space_lim,dtype=float) #space lim (min, max) for the FBM
-    def _autocovariance(self,k,hurst):
+        # stocastic rate constants with units 1/dt
+        self.diffusion_parameter_transition_matrix = diffusion_parameter_transition_matrix
+        # stocastic rate constants with units 1/dt
+        self.hurst_parameter_transition_matrix = hurst_parameter_transition_matrix
+        # probability of the initial state, this approximates the population distribution
+        self.state_probability_diffusion = state_probability_diffusion
+        # probability of the initial state, this approximates the population distribution
+        self.state_probability_hurst = state_probability_hurst
+        # space lim (min, max) for the FBM
+        self.space_lim = np.array(space_lim, dtype=float)
+        #initialize the autocovariance matrix and the diffusion parameter
+        self._setup()
+
+    def _autocovariance(self, k, hurst):
         '''Autocovariance function for fGn
 
         Parameters:
@@ -91,50 +103,59 @@ class FBM_BP:
             Hurst parameter
         diff_a : float
             Diffusion coefficient related to the Hurst parameter
-        
+
         Returns:
         --------
         float
             Autocovariance function
         '''
         return 0.5*(abs(k - 1) ** (2 * hurst) - 2 * abs(k) ** (2 * hurst) + abs(k + 1) ** (2 * hurst))
+
+
+    def _setup(self) -> None:
+        "setup to avoid recomputation of the autocovariance matrix and diffusion parameter"
+        self._cov = np.zeros(self.n)
+        self._diff_a_n = np.zeros(self.n)
+        self._hurst_n = np.zeros(self.n)
+        # catch if the diffusion or hurst parameter sets are singular
+        if len(self.diffusion_parameter) == 1:
+            self._diff_a_n = np.full(self.n, self.diffusion_parameter[0])
+        else:
+            diff_a_start = np.random.choice(
+                self.diffusion_parameter, p=self.state_probability_diffusion)
+            self._diff_a_n[0] = diff_a_start
+            self._diff_a_n[1:] = MCMC_state_selection(np.where(self.diffusion_parameter == diff_a_start)[
+                                                     0][0], self.diffusion_parameter_transition_matrix, self.diffusion_parameter, self.n-1)
+        if len(self.hurst_parameter) == 1:
+            self._hurst_n = np.full(self.n, self.hurst_parameter[0])
+        else:
+            hurst_start = np.random.choice(
+                self.hurst_parameter, p=self.state_probability_hurst)
+            self._hurst_n[0] = hurst_start
+            self._hurst_n[1:] = MCMC_state_selection(np.where(self.hurst_parameter == hurst_start)[
+                                                    0][0], self.hurst_parameter_transition_matrix, self.hurst_parameter, self.n-1)
+        for i in range(self.n):
+            self._cov[i] = self._autocovariance(i, self._hurst_n[i])
+
+
     def fbm(self):
         fgn = np.zeros(self.n)
         fbm_store = np.zeros(self.n)
         phi = np.zeros(self.n)
         psi = np.zeros(self.n)
-        diff_a_n = np.zeros(self.n)
-        hurst_n = np.zeros(self.n)
-        #generate the autocovariance matrix using the chosen diffusion and hurst parameter
-        self._cov = np.zeros(self.n)
-        #catch if the diffusion or hurst parameter sets are singular
-        if len(self.diffusion_parameter) == 1:
-            diff_a_n = np.full(self.n, self.diffusion_parameter[0])
-        else:
-            diff_a_start = np.random.choice(self.diffusion_parameter, p=self.state_probability_diffusion)
-            diff_a_n[0] = diff_a_start
-            diff_a_n[1:] = MCMC_state_selection(np.where(self.diffusion_parameter == diff_a_start)[0][0], self.diffusion_parameter_transition_matrix, self.diffusion_parameter, self.n-1)
-        if len(self.hurst_parameter) == 1:
-            hurst_n = np.full(self.n, self.hurst_parameter[0])
-        else:
-            hurst_start = np.random.choice(self.hurst_parameter, p=self.state_probability_hurst)
-            hurst_n[0] = hurst_start
-            hurst_n[1:] = MCMC_state_selection(np.where(self.hurst_parameter == hurst_start)[0][0], self.hurst_parameter_transition_matrix, self.hurst_parameter, self.n-1)
-        for i in range(self.n):
-            self._cov[i] = self._autocovariance(i,hurst_n[i])
-
-        #construct a gaussian noise vector
-        gn = np.random.normal(0, 1, self.n)*np.sqrt(self.dt*2*diff_a_n)*(self.dt**(2*hurst_n)) 
-        #catch is all hurst are 0.5 then use the gaussian noise vector corresponding to the scale defined by the diffusion parameter
-        if np.all(hurst_n == 0.5):
-            #each gn is then pulled from a normal distribution with mean 0 and standard deviation diff_a_n
-            #ignore the fbm calculations but keep the reflection
+        # construct a gaussian noise vector
+        gn = np.random.normal(0, 1, self.n) * \
+            np.sqrt(self.dt*2*self._diff_a_n)*(self.dt**(2*self._hurst_n))
+        # catch is all hurst are 0.5 then use the gaussian noise vector corresponding to the scale defined by the diffusion parameter
+        if np.all(self._hurst_n == 0.5):
+            # each gn is then pulled from a normal distribution with mean 0 and standard deviation diff_a_n
+            # ignore the fbm calculations but keep the reflection
             for i in range(1, self.n):
                 fbm_candidate = fbm_store[i - 1] + gn[i]
-                #check if this is outside the space limit by using the reflecting boundary condition
-                fbm_store[i] = _boundary_conditions(fbm_store[i - 1], fbm_candidate, self.space_lim, 'reflecting')
+                # check if this is outside the space limit by using the reflecting boundary condition
+                fbm_store[i] = _boundary_conditions(
+                    fbm_store[i - 1], fbm_candidate, self.space_lim, 'reflecting')
             return fbm_store
-
 
         fbm_store[0] = 0
         fgn[0] = gn[0]
@@ -153,17 +174,19 @@ class FBM_BP:
             for j in range(i):
                 fgn[i] += phi[j] * fgn[i - j - 1]
             fgn[i] += np.sqrt(np.abs(v)) * gn[i]
-            #add to the fbm
+            # add to the fbm
             fbm_candidate = fbm_store[i - 1] + fgn[i]
 
-            #check if this is outside the space limit by using the reflecting boundary condition
-            fbm_store[i] = _boundary_conditions(fbm_store[i - 1], fbm_candidate, self.space_lim, 'reflecting')
+            # check if this is outside the space limit by using the reflecting boundary condition
+            fbm_store[i] = _boundary_conditions(
+                fbm_store[i - 1], fbm_candidate, self.space_lim, 'reflecting')
             if fbm_store[i] != fbm_candidate:
-                #update the fgn based on the new difference
+                # update the fgn based on the new difference
                 fgn[i] = fbm_store[i] - fbm_store[i - 1]
-        return fbm_store 
+        return fbm_store
 
-def _boundary_conditions(fbm_store_last:float, fbm_candidate:float, space_lim:np.ndarray,condition_type:str):
+
+def _boundary_conditions(fbm_store_last: float, fbm_candidate: float, space_lim: np.ndarray, condition_type: str):
     '''Boundary conditions for the FBM
 
     Parameters:
@@ -181,62 +204,66 @@ def _boundary_conditions(fbm_store_last:float, fbm_candidate:float, space_lim:np
     float
         New value of the FBM
     '''
-    #check if the condition type is valid
+    # check if the condition type is valid
     if condition_type not in BOUNDARY_CONDITIONS:
-        raise ValueError('Invalid condition type: ' + condition_type +"! Must be one of: " + str(BOUNDARY_CONDITIONS.keys()))
+        raise ValueError('Invalid condition type: ' + condition_type +
+                         "! Must be one of: " + str(BOUNDARY_CONDITIONS.keys()))
     return BOUNDARY_CONDITIONS[condition_type](fbm_store_last, fbm_candidate, space_lim)
 
-#run tests if this is the main module
+# run tests if this is the main module
+
 
 if __name__ == "__main__":
 
-    #test the MCMC_state_selection function
+    # test the MCMC_state_selection function
     # initialize the transition matrix
     transition_matrix = np.array([[5.7, 0.035],
-                                [.25, 5.7]])
+                                  [.25, 5.7]])
     # initialize the possible states
     possible_states = np.array([1, 2])
     # initialize the number of iterations
     n = 50000
     # initialize the initial state index
     initial_state_index = 1
-    state_select = MCMC_state_selection(initial_state_index, transition_matrix, possible_states, n)
-    #find the probability of each state
+    state_select = MCMC_state_selection(
+        initial_state_index, transition_matrix, possible_states, n)
+    # find the probability of each state
     state_probability = np.zeros(len(possible_states))
     for i in range(len(possible_states)):
         state_probability[i] = np.sum(state_select == possible_states[i])/n
-    #compare the population distribution with the state probability distribution
+    # compare the population distribution with the state probability distribution
     total_rate = np.sum(transition_matrix)
-    #add the column of the transition matrix and divide by the total rate
+    # add the column of the transition matrix and divide by the total rate
     true_state_probability = np.sum(transition_matrix, axis=0)/total_rate
-    plt.bar(possible_states, state_probability, label='State probability distribution', alpha=0.9)
-    #plt.bar(possible_states, true_state_probability, label='Population distribution', alpha=0.5)
+    plt.bar(possible_states, state_probability,
+            label='State probability distribution', alpha=0.9)
+    # plt.bar(possible_states, true_state_probability, label='Population distribution', alpha=0.5)
     plt.xlabel('State')
     plt.ylabel('Probability')
     plt.title('State probability distribution')
     plt.legend()
-    plt.show()  
+    plt.show()
 
-    #find the probability of switching from state 1 to state 2 at each iteration
+    # find the probability of switching from state 1 to state 2 at each iteration
     state_1_to_2 = np.zeros(n) - 1
     for i in range(n-1):
         if state_select[i] == 1 and state_select[i+1] == 2:
             state_1_to_2[i] = 1
         elif state_select[i] == 1 and state_select[i+1] == 1:
             state_1_to_2[i] = 0
-    #find the probability of switching from state 2 to state 1 at each iteration
-    state_2_to_1 = np.zeros(n) -1
+    # find the probability of switching from state 2 to state 1 at each iteration
+    state_2_to_1 = np.zeros(n) - 1
     for i in range(n-1):
         if state_select[i] == 2 and state_select[i+1] == 1:
             state_2_to_1[i] = 1
         elif state_select[i] == 2 and state_select[i+1] == 2:
             state_2_to_1[i] = 0
-    #print the probability of switching from state 1 to state 2 ignore 0s
-    print('Probability of switching from state 1 to state 2: ' + str(np.sum(state_1_to_2[state_1_to_2 != -1])/np.sum(state_1_to_2 != -1)))
-    #print the probability of switching from state 2 to state 1 ignore 0s
-    print('Probability of switching from state 2 to state 1: ' + str(np.sum(state_2_to_1[state_2_to_1 != -1])/np.sum(state_2_to_1 != -1)))
-
-
+    # print the probability of switching from state 1 to state 2 ignore 0s
+    print('Probability of switching from state 1 to state 2: ' +
+          str(np.sum(state_1_to_2[state_1_to_2 != -1])/np.sum(state_1_to_2 != -1)))
+    # print the probability of switching from state 2 to state 1 ignore 0s
+    print('Probability of switching from state 2 to state 1: ' +
+          str(np.sum(state_2_to_1[state_2_to_1 != -1])/np.sum(state_2_to_1 != -1)))
 
     # # test the FBM_BP class
     # n = 2000
@@ -269,7 +296,7 @@ if __name__ == "__main__":
     # # initialize the number of iterations
     # n = 100
     # # initialize the initial state index
-    # initial_state_index = 1 
+    # initial_state_index = 1
 
     # # test the MCMC_state_selection function
     # state_selection = MCMC_state_selection(initial_state_index, transition_matrix, possible_states, n)
@@ -319,13 +346,13 @@ if __name__ == "__main__":
 
     # # #test the MSD calculation
     # import sys
-    # sys.path.append('/Users/baljyot/Documents/CODE/GitHub_t2/Baljyot_EXP_RPOC/Scripts') 
+    # sys.path.append('/Users/baljyot/Documents/CODE/GitHub_t2/Baljyot_EXP_RPOC/Scripts')
     # sys.path.append('/Users/baljyot/Documents/CODE/GitHub_t2/Baljyot_EXP_RPOC/Scripts/src')
     # from SMT_Analysis_BP.helpers.analysisFunctions.MSD_Utils import MSD_Calculations_Track_Dict
     # #make a 2D FBM by making two 1D FBM and then combining them
     # n = 1000
     # dt = 1
-    # #singular 
+    # #singular
     # diffusion_parameters = np.array([2])
     # hurst_parameters = np.array([0.5])
     # diffusion_parameter_transition_matrix = np.array([[1]])
@@ -336,7 +363,7 @@ if __name__ == "__main__":
     # fbm_bp = FBM_BP(n, dt, diffusion_parameters, hurst_parameters, diffusion_parameter_transition_matrix, hurst_parameter_transition_matrix, state_probability_diffusion, state_probability_hurst, space_lim)
     # # test the fbm method
     # fbm_x = fbm_bp.fbm()
-    # fbm_bp = FBM_BP(n, dt, diffusion_parameters, hurst_parameters, diffusion_parameter_transition_matrix, hurst_parameter_transition_matrix, state_probability_diffusion, state_probability_hurst, space_lim) 
+    # fbm_bp = FBM_BP(n, dt, diffusion_parameters, hurst_parameters, diffusion_parameter_transition_matrix, hurst_parameter_transition_matrix, state_probability_diffusion, state_probability_hurst, space_lim)
     # fbm_y = fbm_bp.fbm()
     # #plot the fbm
     # plt.plot(fbm_x,fbm_y,'.-')
@@ -372,6 +399,5 @@ if __name__ == "__main__":
     # plt.xscale('log')
     # plt.yscale('log')
     # plt.show()
-    
 
     pass
