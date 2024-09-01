7eb859d006978c9af99473c190eb1f57c811ac53
Baljyot Parmar <joemans3@users.noreply.github.com>
2024-09-01 01:27:23 -0400

    Tool Addition: Utility copy config file to currect directory

--- SMS_BP/run_cell_simulation.py
+++ SMS_BP/run_cell_simulation.py
@@ -61,6 +61,53 @@ def main_CLI():
     )
 
 
+# utility CLI tool to create a copy of the sim_config.json file in the current directory from which the tool is run
+def create_config():
+    """
+    Create a copy of the sim_config.json file in the current directory
+
+    Usage:
+        python run_cell_simulation.py create_config --output_path <output_path>
+    Options:
+        --output_path   Path to the output directory
+    """
+
+    parser = argparse.ArgumentParser(
+        description="CLI tool to create a copy of the sim_config.json file in the current directory"
+    )
+    parser.add_argument("--output_path", help="Path to the output directory")
+    args = parser.parse_args()
+
+    # check if the output path is provided and is a valid directory
+    if args.output_path and not os.path.isdir(args.output_path):
+        # make the directory if it does not exist but tell the user
+        print("Creating directory structure: ", args.output_path)
+        os.makedirs(args.output_path)
+
+    # find the project directory
+    project_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # find the config file
+    config_file = os.path.join(project_directory, "SMS_BP", "sim_config.json")
+
+    # if the output path is provided append it to the config file
+    if args.output_path:
+        TEMP_CONFIG_FILE = os.path.join(args.output_path, "sim_config.json")
+    else:
+        TEMP_CONFIG_FILE = os.path.join(os.getcwd(), "sim_config.json")
+
+    # check if the config file exists in the current directory
+    if os.path.exists(TEMP_CONFIG_FILE):
+        # warn the user that the file already exists
+        print(
+            f"Warning: Configuration file already exists in the current directory: {TEMP_CONFIG_FILE}"
+        )
+        # stopping and do nothing
+        return
+    # copy the config file to the current directory
+    os.system(f"cp {config_file} {TEMP_CONFIG_FILE}")
+
+
 # make a new function which handles running this script without CLI arguments
 
 
